1c7ff47e0dc3930109a0479e07812e94d036a999
MADHAV NANDAN <mnandan06@gmail.com>
2020-05-31 23:17:17 +0530

    Day16 program

---
+++ 30_day_of_code/Day16/Solution.swift
@@ -0,0 +1,9 @@
+
+func stringToInt(inputString: String) throws -> Int {
+
+   guard let result = Int(inputString) else {
+       throw StringToIntTypecastingError.BadString
+   }
+   return result
+
+}
